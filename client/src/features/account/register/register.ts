1463986a78de7a7a181ca10e9bed4a8928dec658
TnFlow <alexmedina270702@gmail.com>
2025-10-07 14:12:25 -0600

    Added Register form, Parent to Child communication (also Child to Parent)

---
+++ client/src/features/account/register/register.ts
@@ -0,0 +1,24 @@
+import { Component, input, output } from '@angular/core';
+import { RegisterCreds } from '../../../types/registerCreds';
+import { FormsModule } from '@angular/forms';
+import { User } from '../../../types/user';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css'
+})
+export class Register {
+  membersFromHome = input.required<User[]>();
+  cancelRegister = output<boolean>();
+  protected creds = {} as RegisterCreds;
+
+  register(): void {
+    console.log(this.creds);
+  }
+
+  cancel(): void {
+    this.cancelRegister.emit(false);
+  }
+}
