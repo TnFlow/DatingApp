04eed74160ed59101576a224b93b7cfe9e325eeb
TnFlow <alexmedina270702@gmail.com>
2025-12-06 13:17:33 -0600

    Added Member Detail Child Components and Member Resolver

--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -1,32 +1,35 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MembersService } from '../../../core/services/members-service';
-import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { Member } from '../../../types/member';
-import { Observable } from 'rxjs';
+import { filter, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-member-detail',
-  imports: [AsyncPipe, RouterLink, RouterLinkActive],
+  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css'
 })
 export class MemberDetail implements OnInit {
   private membersService = inject(MembersService);
   private route = inject(ActivatedRoute);
-  protected member$?: Observable<Member>;
+  private router = inject(Router);
+  protected member = signal<Member | undefined>(undefined);
+  protected title = signal<string | undefined>("Profile");
 
   ngOnInit(): void {
-    this.member$ = this.loadMember();
-  }
+    this.route.data.subscribe({
+        next: data => this.member.set(data["member"])
+    });
+    this.title.set(this.route.firstChild?.snapshot?.title);
 
-  loadMember(): Observable<Member> | undefined {
-    const id = this.route.snapshot.paramMap.get("id");
-
-    if (id) {
-      return this.membersService.getMember(id);
-    }
-
-    return;
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe({
+      next: () => {
+        this.title.set(this.route.firstChild?.snapshot?.title);
+      }
+    });
   }
 }
