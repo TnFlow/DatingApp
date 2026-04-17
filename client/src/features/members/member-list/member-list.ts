3050622476d1129dbf14b07981f790facede30fb
TnFlow <alexmedina270702@gmail.com>
2026-04-16 21:18:55 -0600

    Added page change, added signal to avoid paginator component reload

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,22 +1,39 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MembersService } from '../../../core/services/members-service';
 import { Member } from '../../../types/member';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { MemberCard } from "../member-card/member-card";
 import { PaginationResult } from '../../../types/paginationMetadata';
+import { Paginator } from "../../../shared/paginator/paginator";
 
 @Component({
   selector: 'app-member-list',
-  imports: [AsyncPipe, MemberCard],
+  imports: [AsyncPipe, MemberCard, Paginator],
   templateUrl: './member-list.html',
   styleUrl: './member-list.css'
 })
-export class MemberList {
+export class MemberList implements OnInit {
   private membersService = inject(MembersService);
-  protected paginatedMembers$: Observable<PaginationResult<Member>>;
+  protected paginatedMembers = signal<PaginationResult<Member> | null>(null);
+  pageNumber = 1;
+  pageSize = 5;
 
-  constructor() {
-    this.paginatedMembers$ = this.membersService.getMembers();
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.membersService.getMembers(this.pageNumber, this.pageSize).subscribe({
+      next: result => {
+        this.paginatedMembers.set(result);
+      }
+    })
+  }
+
+  onPageChange(event: { pageNumber: number, pageSize: number }) {
+    this.pageNumber = event.pageNumber;
+    this.pageSize = event.pageSize;
+    this.loadMembers();
   }
 }
