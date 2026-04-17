c57d389ead4612474ab09544da36c27328c9f6a2
TnFlow <alexmedina270702@gmail.com>
2026-04-16 21:26:42 -0600

    Added filter modal 2/2, added age gender filters to modal, added sorting to API

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -8,7 +8,7 @@ import { FilterModal } from '../filter-modal/filter-modal';
 
 @Component({
   selector: 'app-member-list',
-  imports: [MemberCard, Paginator],
+  imports: [MemberCard, Paginator, FilterModal],
   templateUrl: './member-list.html',
   styleUrl: './member-list.css'
 })
@@ -42,5 +42,15 @@ export class MemberList implements OnInit {
 
   onClose() {
     console.log('Modal closed');
+  }
+
+  onFilterChange(data: MemberParams) {
+    this.memberParams = data;
+    this.loadMembers();
+  }
+
+  resetFilters() {
+    this.memberParams = new MemberParams();
+    this.loadMembers();
   }
 }
