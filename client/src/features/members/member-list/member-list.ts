01b7524153f24f64848d4b42aceb4fc06830eb1f
TnFlow <alexmedina270702@gmail.com>
2026-04-16 21:24:02 -0600

    Added filtering API call from SPA, added filter modal 1/2

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,30 +1,29 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { MembersService } from '../../../core/services/members-service';
-import { Member } from '../../../types/member';
-import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { Member, MemberParams } from '../../../types/member';
 import { MemberCard } from "../member-card/member-card";
 import { PaginationResult } from '../../../types/paginationMetadata';
 import { Paginator } from "../../../shared/paginator/paginator";
+import { FilterModal } from '../filter-modal/filter-modal';
 
 @Component({
   selector: 'app-member-list',
-  imports: [AsyncPipe, MemberCard, Paginator],
+  imports: [MemberCard, Paginator],
   templateUrl: './member-list.html',
   styleUrl: './member-list.css'
 })
 export class MemberList implements OnInit {
+  @ViewChild('filterModal') modal!: FilterModal;
   private membersService = inject(MembersService);
   protected paginatedMembers = signal<PaginationResult<Member> | null>(null);
-  pageNumber = 1;
-  pageSize = 5;
+  protected memberParams = new MemberParams();
 
   ngOnInit(): void {
     this.loadMembers();
   }
 
   loadMembers() {
-    this.membersService.getMembers(this.pageNumber, this.pageSize).subscribe({
+    this.membersService.getMembers(this.memberParams).subscribe({
       next: result => {
         this.paginatedMembers.set(result);
       }
@@ -32,8 +31,16 @@ export class MemberList implements OnInit {
   }
 
   onPageChange(event: { pageNumber: number, pageSize: number }) {
-    this.pageNumber = event.pageNumber;
-    this.pageSize = event.pageSize;
+    this.memberParams.pageNumber = event.pageNumber;
+    this.memberParams.pageSize = event.pageSize;
     this.loadMembers();
+  }
+
+  openModal() {
+    this.modal.open();
+  }
+
+  onClose() {
+    console.log('Modal closed');
   }
 }
