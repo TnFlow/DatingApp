19f37fd56eb24a0a421a6ca9398d08b14a6436e7
TnFlow <alexmedina270702@gmail.com>
2026-06-14 15:29:58 -0600

    Added message thread to client, chat bubbles and send message text box

--- client/src/features/member-messages/member-messages.ts
+++ client/src/features/member-messages/member-messages.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MessagesService } from '../../core/services/messages-service';
+import { MembersService } from '../../core/services/members-service';
+import { Message } from '../../types/message';
+import { DatePipe } from '@angular/common';
+import { TimeAgoPipe } from '../../core/pipes/time-ago-pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-member-messages',
-  imports: [],
+  imports: [DatePipe, TimeAgoPipe, FormsModule],
   templateUrl: './member-messages.html',
   styleUrl: './member-messages.css'
 })
-export class MemberMessages {
+export class MemberMessages implements OnInit {
+  private messagesService = inject(MessagesService);
+  private membersService = inject(MembersService);
+  protected messages = signal<Message[]>([]);
+  protected messageContent = '';
 
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    const memberId = this.membersService.member()?.id;
+    if (memberId) {
+      this.messagesService.getMessageThread(memberId).subscribe({
+        next: messages => this.messages.set(messages.map(message => ({
+          ...message,
+          currentUserSender: message.senderId !== memberId
+        })))
+      })
+    }
+  }
+
+  sendMessage() {
+    const recipientId = this.membersService.member()?.id;
+    if (!recipientId) return;
+    this.messagesService.sendMessage(recipientId, this.messageContent).subscribe({
+      next: message => {
+        this.messages.update(messages => {
+          message.currentUserSender = true;
+          return [...messages, message];
+        });
+        this.messageContent = '';
+      }
+    })
+  }
 }
