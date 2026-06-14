304e9b6f67f605895c327c1318a0abe6f48a01df
TnFlow <alexmedina270702@gmail.com>
2026-06-14 15:03:18 -0600

    Added initial migration for Message, Updated migration for Message and Added message repository

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,16 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { LoginCreds, User } from '../../types/user';
+import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { Observable, tap } from 'rxjs';
-import { RegisterCreds } from '../../types/registerCreds';
+import { environment } from '../../environments/environment';
+import { LikesService } from './likes-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   private http = inject(HttpClient);
+  private likesService = inject(LikesService);
   currentUser = signal<User | null>(null);
-  baseUrl = "https://localhost:5001/api/";
+  baseUrl = environment.apiUrl;
 
   register(creds: RegisterCreds): Observable<User> {
     return this.http.post<User>(this.baseUrl + "account/register", creds).pipe(
@@ -35,10 +37,13 @@ export class AccountService {
   setCurrentUser(user: User) {
     localStorage.setItem("user", JSON.stringify(user));
     this.currentUser.set(user);
+    this.likesService.getLikeIds();
   }
 
   logout() {
     localStorage.removeItem("user");
+    localStorage.removeItem("filters");
+    this.likesService.clearLikeIds();
     this.currentUser.set(null);
   }
 }
--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -10,3 +10,13 @@ export type LoginCreds = {
   email: string;
   password: string;
 }
+
+export type RegisterCreds = {
+  email: string;
+  displayName: string;
+  password: string;
+  gender: string;
+  birthDay: string;
+  city: string;
+  country: string;
+}
